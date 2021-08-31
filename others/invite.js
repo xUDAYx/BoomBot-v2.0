07fcf50457d1d9145b7cd2b0899f6a8db92a03f0
Uday Jawheri <69415940+xUDAYx@users.noreply.github.com>
2021-08-31 19:14:19 +0530

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -10,7 +10,7 @@ module.exports = {
       .setTitle("Add us to your server!")
       .setDescription("Love using BoomBot? Great, Thank you! Consider adding it to your server")
       .setColor("#F0EAD6")
-      .setAuthor('Harmony','https://cdn.discordapp.com/attachments/608531899429814283/882241348672040990/raze_bot_disk.gif')
+      .setAuthor('BoomBot','https://cdn.discordapp.com/attachments/608531899429814283/882241348672040990/raze_bot_disk.gif')
       .setThumbnail(message.guild.iconURL())
       .addField(`Use the following link to add Boombot to your discord server`, 'https://discord.com/api/oauth2/authorize?client_id=818450510343438336&permissions=8&scope=bot', true)
 
